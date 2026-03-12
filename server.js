c8587fd15507f892d66716f5c8659475258084ab
Aliaakib <aliaakibbukhari110@gmail.com>
2026-03-12 13:21:05 +0530

    updated 12 march 26 backend

--- server.js
+++ server.js
@@ -126,3 +126,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+// app.listen(PORT,'0.0.0.0', () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
